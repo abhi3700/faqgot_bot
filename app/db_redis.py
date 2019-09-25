dfaddec502739d34b1ae0bf7514c85aa52a60e32
Abhijit Roy <dalvath3700@gmail.com>
2019-09-25 19:59:35 +0530

    redis DB added user details, quiz details

--- app/db_redis.py
+++ app/db_redis.py
@@ -27,20 +27,12 @@ c_list = df.c.tolist()
 d_list = df.d.tolist()
 # -----------------------------------------------------------------------------
 """Setting the database"""
-for i in range(10):
-    r.hset("quiz", str(i), json.dumps(dict(que= que_list[i], ans= ans_list[i], a= a_list[i], b= b_list[i], c= c_list[i], d= d_list[i])))
-
-
-# -----------------------------------------------------------------------------
-"""Finding the phone no. if username exists"""
-# key_phone = ""
-# for k in keys_list:
-#     # print(k.decode('utf-8'))
-#     dict_nested2_val2 = json.loads(r.get(k.decode('utf-8')))
-#     if dict_nested2_val2['username'] == 'abhi3701':
-#         key_phone = k.decode('utf-8')
-
-# print(key_phone)
+# quiz
+# for i in range(10):
+#     r.hset("quiz", str(i), json.dumps(dict(que= que_list[i], ans= ans_list[i], a= a_list[i], b= b_list[i], c= c_list[i], d= d_list[i])))
+# # user details
+r.set("918145634656", json.dumps(dict(username="abhi3701",correct_count=4, incorrect_count=4, total_attempt=8,score=50)))
+r.set("918264433324", json.dumps(dict(username="abhi3702",correct_count=4, incorrect_count=4, total_attempt=8,score=50)))
 
 # -----------------------------------------------------------------------------
 """delete all stored keys"""
@@ -50,5 +42,30 @@ for i in range(10):
 # -----------------------------------------------------------------------------
 """print all keys"""
 # quiz
-for i in range(10):
-    print(json.loads(r.hget("quiz", str(i))))
+# for i in range(10):
+#     print(json.loads(r.hget("quiz", str(i))))
+
+keys_list = r.keys()
+# print(keys_list.remove(b'quiz'))
+keys_list.remove(b'quiz')
+# print(keys_list)
+
+"""print user's details"""
+for k in keys_list:
+    k_decoded = k.decode('utf-8')
+    print(json.loads(r.get(k_decoded)))
+    print(json.loads(r.get(k_decoded))['username'])
+    print(json.loads(r.get(k_decoded))['correct_count'])
+    print(json.loads(r.get(k_decoded))['incorrect_count'])
+    print(json.loads(r.get(k_decoded))['total_attempt'])
+    print(json.loads(r.get(k_decoded))['score'])
+# -----------------------------------------------------------------------------
+"""Finding the phone no. if username exists"""
+key_phone = ""
+for k in keys_list:
+    # print(k.decode('utf-8'))
+    dict_nested2_val2 = json.loads(r.get(k.decode('utf-8')))
+    if dict_nested2_val2['username'] == 'abhi3701':
+        key_phone = k.decode('utf-8')
+
+print(key_phone)
