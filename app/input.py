612cac9c1d4f369ee1829128e645847cf5df60ae
Abhijit Roy <dalvath3700@gmail.com>
2019-09-26 18:40:43 +0530

    REDIS DB_URL modified

--- app/input.py
+++ app/input.py
@@ -1,4 +1,4 @@
 API_key = "950270851:AAH0VT6pT411VGv7lqNitUPqPegFCHPMoqc"
 
 # Capture using `$ heroku config | grep REDIS` from the terminal at App's root dir
-REDIS_URL = 'redis://h:p40a7ec51aeb50b281429b3fbfa951abf65d461b65dac6d4e867fb65ef290004a@ec2-18-203-184-96.eu-west-1.compute.amazonaws.com:12259'
+REDIS_URL = 'redis://h:p0332252301e254841335538e41ba40546dad23047b490f59a9617afce339f73c@ec2-3-248-24-66.eu-west-1.compute.amazonaws.com:24839'
