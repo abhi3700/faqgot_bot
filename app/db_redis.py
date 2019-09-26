a9fa3d22de96fcbe012dd14950aa32cbc3ae5da7
Abhijit Roy <dalvath3700@gmail.com>
2019-09-26 18:40:10 +0530

    final python code added

--- app/db_redis.py
+++ app/db_redis.py
@@ -12,6 +12,11 @@ from input import REDIS_URL
 # define Redis database
 r = redis.from_url(REDIS_URL)
 
+# -----------------------------------------------------------------------------
+"""delete all stored keys"""
+for k in r.keys():
+    r.delete(k)
+
 print(r.keys())
 
 # ---------------------------------------------------------------
@@ -28,44 +33,63 @@ d_list = df.d.tolist()
 # -----------------------------------------------------------------------------
 """Setting the database"""
 # quiz
-for i in range(1,10+1):     # loop from 1 to 10
-    r.hset("quiz", str(i-1), json.dumps(dict(que= que_list[i-1], ans= ans_list[i-1], a= a_list[i-1], b= b_list[i-1], c= c_list[i-1], d= d_list[i-1])))
-# # user details
-r.set("918145634656", json.dumps(dict(username="abhi3701",correct_count=4, incorrect_count=4, total_attempt=8,score=50)))
-r.set("918264433324", json.dumps(dict(username="abhi3702",correct_count=4, incorrect_count=4, total_attempt=8,score=50)))
+for i in range(10):     # loop from 1 to 10
+    r.hset("quiz", str(i+1), json.dumps(dict(que= que_list[i], ans= ans_list[i], a= a_list[i], b= b_list[i], c= c_list[i], d= d_list[i])))
 
-# -----------------------------------------------------------------------------
-"""delete all stored keys"""
-# for k in r.keys():
-#     r.delete(k)
+# user details
+r.hset("91834234567", "user", json.dumps(dict(username="abhi3703", total_attempt=-1)))
+r.hset("91834234567", "correct", json.dumps(dict(count= 0)))
+r.hset("91834234567", "incorrect", json.dumps(dict(count= 0)))
+r.hset("91834234567", "score", 0)
+
+r.hset("91814563465", "user", json.dumps(dict(username="abhi3702", total_attempt=-1)))
+r.hset("91814563465", "correct", json.dumps(dict(count= 0)))
+r.hset("91814563465", "incorrect", json.dumps(dict(count= 0)))
+r.hset("91814563465", "score", 0)
+
+r.hset("91826443332", "user", json.dumps(dict(username="abhi3701", total_attempt=-1)))
+r.hset("91826443332", "correct", json.dumps(dict(count= 0)))
+r.hset("91826443332", "incorrect", json.dumps(dict(count= 0)))
+r.hset("91826443332", "score", 0)
+
+# update the correct count and check if it is affecting other trees - incorrect, score
+r.hset("91834234567", "correct", json.dumps(dict(count= 34)))
 
 # -----------------------------------------------------------------------------
 """print all keys"""
 # quiz
 for i in range(10):
-    print(json.loads(r.hget("quiz", str(i))))
+    print(json.loads(r.hget("quiz", str(i+1))))
 
 keys_list = r.keys()
-# print(keys_list.remove(b'quiz'))
 keys_list.remove(b'quiz')
 # print(keys_list)
 
 """print user's details"""
 for k in keys_list:
     k_decoded = k.decode('utf-8')
-    print(json.loads(r.get(k_decoded)))
-    print(json.loads(r.get(k_decoded))['username'])
-    print(json.loads(r.get(k_decoded))['correct_count'])
-    print(json.loads(r.get(k_decoded))['incorrect_count'])
-    print(json.loads(r.get(k_decoded))['total_attempt'])
-    print(json.loads(r.get(k_decoded))['score'])
+    print('==========================')
+    print(k_decoded)
+    print(json.loads(r.hget(k_decoded, "user")))
+    print(json.loads(r.hget(k_decoded, "user"))['username'])
+    print(json.loads(r.hget(k_decoded, "user"))['total_attempt'])
+    print(json.loads(r.hget(k_decoded, "correct"))['count'])
+    print(json.loads(r.hget(k_decoded, "incorrect"))['count'])
+    print(json.loads(r.hget(k_decoded, "score")))
+
 # -----------------------------------------------------------------------------
 """Finding the phone no. if username exists"""
-key_phone = ""
-for k in keys_list:
-    # print(k.decode('utf-8'))
-    dict_nested2_val2 = json.loads(r.get(k.decode('utf-8')))
-    if dict_nested2_val2['username'] == 'abhi3701':
-        key_phone = k.decode('utf-8')
+# key_phone = ""
+# for k in keys_list:
+#     # print(k.decode('utf-8'))
+#     dict_nested2_val2 = json.loads(r.get(k.decode('utf-8')))
+#     if dict_nested2_val2['username'] == 'abhi3701':
+#         key_phone = k.decode('utf-8')
 
-print(key_phone)
+# print(key_phone)
+
+
+# curr_ans = json.loads(r.hget("quiz", str(0)))['ans']
+# print(curr_ans[0].lower())
+
+print(r.keys())
