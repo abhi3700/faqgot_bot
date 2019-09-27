6a212977336fb4f8ba19e30290479d4a7a52d12b
Abhijit Roy <dalvath3700@gmail.com>
2019-09-27 16:29:57 +0530

    quiz_count var added

--- app/input.py
+++ app/input.py
@@ -2,3 +2,6 @@ API_key = "950270851:AAH0VT6pT411VGv7lqNitUPqPegFCHPMoqc"
 
 # Capture using `$ heroku config | grep REDIS` from the terminal at App's root dir
 REDIS_URL = 'redis://h:p0332252301e254841335538e41ba40546dad23047b490f59a9617afce339f73c@ec2-3-248-24-66.eu-west-1.compute.amazonaws.com:24839'
+
+# Quiz question count
+QUIZ_COUNT = 60
