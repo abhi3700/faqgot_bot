21483a487310b54725af79be66b94a72babfd64b
Abhijit Roy <dalvath3700@gmail.com>
2019-09-27 16:30:04 +0530

    practice updated

--- practice.py
+++ practice.py
@@ -1,6 +1,2 @@
-import json
-
-item_dict = dict(stats= json.dumps(dict(correct_count= 0)))
-print(item_dict)
-print(item_dict.get('stats'))
-print(json.loads(item_dict['stats']).get('correct_count'))
+instr = 'Which band made a cameo appearance at the Purple Wedding?\nhttps://github.com/abhi3700/dummy_data_DONT_DELETE/blob/master/got_img12.jpg'
+print(instr)
