f74c4febe7898974d49d7c863628e3c15323f964
Abhijit Roy <dalvath3700@gmail.com>
2019-09-30 15:44:40 +0530

    quiz_count --> 60 to 91

--- app/input.py
+++ app/input.py
@@ -4,4 +4,4 @@ API_key = "950270851:AAH0VT6pT411VGv7lqNitUPqPegFCHPMoqc"
 REDIS_URL = 'redis://h:pb8cf3fe9644caf6fdf47dd3223bb7334d70058ecbaa8fb3e91ac41fac82ee3bc@ec2-99-81-89-240.eu-west-1.compute.amazonaws.com:15289'
 
 # Quiz question count
-QUIZ_COUNT = 60
+QUIZ_COUNT = 91
